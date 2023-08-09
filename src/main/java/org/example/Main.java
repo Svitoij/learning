63ab9f96e0f2134429dbc5b7d0ac0634ae718c63
SvitoijPC <svitoij@gmail.com>
2023-08-09 23:22:16 +0300

    4thCommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Main {
     public static void main(String[] args) {
         String firstName = "Olek";
         String lastName = "Sandr";
 
-        System.out.println(firstName + " " + lastName + " ти шо дЕбіл?");
+
+        Period difference = Period.between(LocalDate.of(2000, 8, 12), LocalDate.now());
+        int age = difference.getYears();
+        System.out.println(firstName + " " + lastName + " ти шо дЕбіл? Тобі " + age);
     }
 }
